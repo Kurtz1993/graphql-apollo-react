061d13f0e60b2afe3eb91d3da50696ff2b3d4bdc
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-03 12:44:51 -0500

    Add resolutions resolver

---
+++ imports/api/resolutions/resolvers.js
@@ -0,0 +1,16 @@
+export default {
+  Query: {
+    resolutions() {
+      return [
+        {
+          _id: "1",
+          name: "Get stuff done!",
+        },
+        {
+          _id: "fdafafafdaf",
+          name: "Lose some wieght!",
+        },
+      ];
+    },
+  },
+};
