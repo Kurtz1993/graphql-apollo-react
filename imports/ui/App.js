da4a12937eb58d1f2a1b4b83192e3b5d64482c52
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-03 12:34:53 -0500

    Query resolutions

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,11 +2,26 @@ import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
-const App = ({ data }) => <h1>{data.hi}</h1>;
+const App = ({ data }) => (
+  <div>
+    <h1>{data.hi}</h1>
+
+    <ul>
+      {data.resolutions &&
+        data.resolutions.map(resolution => (
+          <li key={resolution._id}>{resolution.name}</li>
+        ))}
+    </ul>
+  </div>
+);
 
 const hiQuery = gql`
   {
     hi
+    resolutions {
+      _id
+      name
+    }
   }
 `;
 
