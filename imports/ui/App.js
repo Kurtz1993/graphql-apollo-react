b1ff8a90500bb066e383f3d149e25c91b68a2b61
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-03 15:22:24 -0500

    Use ResolutionForm

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
+import ResolutionForm from './ResolutionForm';
+
 const App = ({ data }) => (
   <div>
     <h1>{data.hi}</h1>
-
+    <ResolutionForm />
     <ul>
       {data.resolutions &&
         data.resolutions.map(resolution => (
