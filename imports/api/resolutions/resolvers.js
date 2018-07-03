8cbd7175c1988353a87edfd11132d3124447d255
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-03 15:21:43 -0500

    Add mutation placeholder

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -6,4 +6,12 @@ export default {
       return Resolutions.find({}).fetch();
     },
   },
+  Mutation: {
+    createResolution() {
+      console.log("got here");
+      // const resolutionId = Resolutions.insert({
+      //   name: "Test res",
+      // });
+    },
+  },
 };
