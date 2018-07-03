4fc40210b50b3e2e45e682b4b110d1bad2e582f4
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-03 15:39:19 -0500

    Pass refetch function as a prop to form

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
-import ResolutionForm from './ResolutionForm';
+import ResolutionForm from "./ResolutionForm";
 
 const App = ({ data }) => (
   <div>
     <h1>{data.hi}</h1>
-    <ResolutionForm />
+    <ResolutionForm refetch={data.refetch} />
     <ul>
       {data.resolutions &&
         data.resolutions.map(resolution => (
