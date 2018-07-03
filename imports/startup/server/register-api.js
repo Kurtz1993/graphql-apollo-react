3d91bc56e9052481be6d98bbd0eed8d2b9e7b4b4
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-03 12:16:11 -0500

    Switch code from files

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,1 +1,28 @@
-import ResolutionsSchema from '../../api/resolutions/resolutions.graphql';
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+
+import ResolutionsSchema from "../../api/resolutions/resolutions.graphql";
+
+const testSchema = `
+type Query {
+  hi: String
+}
+`;
+const typeDefs = [testSchema, ResolutionsSchema];
+
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello Level Up";
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({
+  schema
+});
