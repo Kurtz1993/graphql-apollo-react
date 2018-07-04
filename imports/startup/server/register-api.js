33c4b4b7fb82e66f9e96fa93f4f8aaf56bbcc134
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2018-07-04 14:59:16 -0500

    Register user schema and resovlers

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -3,15 +3,18 @@ import { makeExecutableSchema } from "graphql-tools";
 import merge from "lodash/merge";
 
 import ResolutionSchema from "../../api/resolutions/resolution.graphql";
-import ResolutionsResolver from "../../api/resolutions/resolvers";
+import ResolutionsResolvers from "../../api/resolutions/resolvers";
+import UsersResolvers from "../../api/users/resolvers";
+import UserSchema from "../../api/users/user.graphql";
 
 const testSchema = `
 type Query {
   hi: String
   resolutions: [Resolution]
+  user: User
 }
 `;
-const typeDefs = [ResolutionSchema, testSchema];
+const typeDefs = [testSchema, ResolutionSchema, UserSchema];
 
 const testResolver = {
   Query: {
@@ -21,7 +24,7 @@ const testResolver = {
   },
 };
 
-const resolvers = merge(testResolver, ResolutionsResolver);
+const resolvers = merge(testResolver, ResolutionsResolvers, UsersResolvers);
 
 const schema = makeExecutableSchema({
   typeDefs,
